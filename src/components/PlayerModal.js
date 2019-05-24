105f8b62f81fe8062bedf92d87f796b8cc4eabf1
github username <github email address>
2019-05-23 18:53:02 -0700

    changes made to PlayerModal

--- src/components/PlayerModal.js
+++ src/components/PlayerModal.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { Modal } from 'semantic-ui-react';
+import { Modal, Image } from 'semantic-ui-react';
 
 export default class PlayerModal extends Component {
 
   render(){
-    const { firstname, lastname, } = this.props.player;
+    const { firstname, lastname, image, jersey, position } = this.props.player;
     return (
-      <Modal open={this.props.open} onClose={this.props.close} closeIcon>
-        <h1>{firstname}</h1>
+      <Modal size="tiny" open={this.props.open} onClose={this.props.close} closeIcon>
+        <Modal.Header>{firstname + " " + lastname}</Modal.Header>
+        <Modal.Content>
+          <Image wrapped src={image}/>
+          <Modal.Description>
+            Jersey Number: {jersey} <br />
+            Position: {position}
+          </Modal.Description>
+        </Modal.Content>
       </Modal>
     )
   }
